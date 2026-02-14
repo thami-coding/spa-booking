c974259085dce57f961394cbdc61d73bc636bf3c
Thami <thamig125@gmail.com>
2026-02-14 23:41:27 +0200

    refactor remove  routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import Booking from './components/booking/Booking'
-import Login from './components/login/Login'
-import BookingSummary from './components/bookingSummary/BookingSummary'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/book" element={<Booking />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/checkout" element={<BookingSummary />} />
-        {/* <Route path="/payment" element={<App />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
-)
+);
