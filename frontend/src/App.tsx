31bf113abc52aa670deeca5bc54ac9debdcf4bf1
Thami <thamig125@gmail.com>
2026-02-14 23:29:35 +0200

    refactor app add routing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+// import { BrowserRouter, Route, Routes } from "react-router";
+// import HomePage from "./pages/home/HomePage";
+// import BookingPage from "./pages/booking/BookingPage";
+// import LoginPage from "./pages/login/Login";
+
+import AdminDashboard from "./pages/list-dashboard/AdminDashboard";
+
+// import Dashboard from "./pages/dashboard/Dashboard";
+
+// import CheckoutPage from "./pages/checkout/CheckoutPage";
+export default function App() {
+  return (
+//  <Dashboard />
+<AdminDashboard />
+  );
+}
