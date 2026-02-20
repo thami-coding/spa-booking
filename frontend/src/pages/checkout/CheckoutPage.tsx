de34b7eb5b4268f1f9dc772c4cf508086a2b86e3
Thami <thamig125@gmail.com>
2026-02-20 20:54:45 +0200

    add checkout component

--- frontend/src/pages/checkout/CheckoutPage.tsx
+++ frontend/src/pages/checkout/CheckoutPage.tsx
@@ -1,16 +1,58 @@
+import useSWR from "swr";
 import BookingSummary from "../../components/booking-summary/BookingSummary";
+import { usePaymentModal } from "../../hooks/usePaymentMethod";
 import styles from "./CheckoutPage.module.css";
+import { useParams } from "react-router";
+import { getBooking } from "../../api/bookings";
+import Spinner from "../../components/spinner/Spinner";
 
 export default function CheckoutPage() {
+  const params = useParams();
+  const bookingId = params.id!;
+  const { data, isLoading: loading } = useSWR(
+    `/bookings/${bookingId}`,
+    getBooking,
+  );
+  console.log("log", data?.booking);
+
+  const service_id = data?.booking.service_id;
+  const email = data?.booking.email;
+  const { handlePayment, isLoading } = usePaymentModal({
+    email,
+    service_id,
+    bookingId,
+  });
+
   return (
-    <div className={styles.top}>
-      <div className={styles.container}>
-        <h2 className={styles.title}>Booking Summary</h2>
-        <BookingSummary />
-        <button className={styles.payButton} onClick={() => {}}>
-         Pay Now
-        </button>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div className={styles.spinnerContainer}>
+          <Spinner size={50} />
+        </div>
+      ) : (
+        <div className={styles.top}>
+          <div className={styles.container}>
+            <h2 className={styles.title}>Booking Summary</h2>
+            <BookingSummary data={data} />
+            <form>
+              <button
+                type="button"
+                disabled={isLoading}
+                className={styles.payButton}
+                onClick={handlePayment}
+              >
+                {isLoading ? (
+                  <div className={styles.center}>
+                    <Spinner size={17} /> <span>Loading</span>
+                  </div>
+                ) : (
+                  "Pay Now"
+                )}
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
