2c12aefb582058dcaa37129aa3eacb7309e27ea9
Thami <thamig125@gmail.com>
2026-02-20 21:07:21 +0200

    add BookingSummary Component and styles

--- frontend/src/components/booking-summary/BookingSummary.tsx
+++ frontend/src/components/booking-summary/BookingSummary.tsx
@@ -1,79 +1,31 @@
 import Row from "../row/Row";
 import styles from "./BookingSummary.module.css";
+import { format, parseISO, parse } from "date-fns";
+import { capitalizeFirst } from "../../lib/capitalize";
 
-export default function BookingSummary() {
-  const booking = {
-    date: "2026-02-12",
-    time: "10:00",
-    service: "Full body Massage",
-    fullName: "Thamsanqa Gumede",
-    email: "sainttsquared@gmail.com",
-    phone: "0659972023",
-    guests: "1",
-    request: "",
-  };
-
+export default function BookingSummary({ data }) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { appointment_at, id, user_id, is_paid, service_id, ...rest } =
+    data.booking;
+  const [bookedDate, bookedTime] = appointment_at.split("T");
+  const date = format(parseISO(bookedDate), "EEEE, MMMM d, yyyy");
+  const dateObj = parse(bookedTime, "HH:mm:ss", new Date());
+  const time = format(dateObj, "h:mm a");
+  const booking = { date, time, ...rest };
   const details = Object.keys(booking) as Array<keyof typeof booking>;
+
   return (
-    <>
-      {details.map((detail) => {
+    <div className={styles.container}>
+      {details.map((detail, index) => {
         const noRequest = detail === "request" && booking[detail] === "";
+        const detailName = capitalizeFirst(String(detail));
         if (noRequest) return;
-        return <Row detail={booking[detail]} detailName={detail} />;
+        return (
+          <Row key={index} detail={booking[detail]} detailName={detailName} />
+        );
       })}
       <div className={styles.divider} />
-      <Row detail={`R ${Number(350.00).toFixed(2)}`} detailName={"price"} />
-    </>
+      <Row detail={`R ${Number(350.0).toFixed(2)}`} detailName={"Total"} />
+    </div>
   );
-  {
-    /* <div className={styles.summaryItem}>
-        <span>Full Name</span>
-        <strong>{fullName}</strong>
-      </div>
-
-      <div className={styles.summaryItem}>
-        <span>Email Address</span>
-        <strong>{email}</strong>
-      </div>
-
-      <div className={styles.summaryItem}>
-        <span>Phone Number</span>
-        <strong>{phone}</strong>
-      </div>
-
-      <div className={styles.summaryItem}>
-        <span>Service</span>
-        <strong>{service}</strong>
-      </div>
-
-      <div className={styles.summaryItem}>
-        <span>Preferred Date</span>
-        <strong>{preferredDate}</strong>
-      </div>
-
-      <div className={styles.summaryItem}>
-        <span>Preferred Time</span>
-        <strong>{preferredTime}</strong>
-      </div>
-
-      <div className={styles.summaryItem}>
-        <span>Guests</span>
-        <strong>{guests}</strong>
-      </div>
-
-      {specialRequests && (
-        <div className={styles.notes}>
-          <span>Special Requests</span>
-          <p>{specialRequests}</p>
-        </div>
-      )}
-
-      <div className={styles.divider} />
-
-      <div className={styles.priceRow}>
-        <span>Total Price</span>
-        <strong>${price.toFixed(2)}</strong>
-      </div>
-      */
-  }
 }
