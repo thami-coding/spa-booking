44337ec4840d5eb7558a5eee6b53b75479818035
Thami <thamig125@gmail.com>
2026-02-20 20:56:51 +0200

    add login buttons

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,21 +1,33 @@
-import { Link } from "react-router"
-import styles from "./HomePage.module.css"
-
+import { Link } from "react-router";
+import styles from "./HomePage.module.css";
 
 export default function HomePage() {
-    return (
-        <section className={styles.hero}>
-            <video autoPlay muted loop playsInline>
-                <source src="/spa-video.mp4" type="video/mp4" />
-            </video>
-            <div className={styles.heroContent}>
-                <h1>Relax, Rejuvenate, and Restore</h1>
-                <p>
-                    Escape the everyday and indulge in a calming spa experience designed
-                    to refresh your body and mind.
-                </p>
-                <Link to="/book" className={styles.ctaButton}>Book Now</Link>
-            </div>
-        </section>
-    )
+  const token = sessionStorage.getItem("acccess_token");
+  return (
+    <section className={styles.hero}>
+      {!token && (
+        <div className={styles.linkContainer}>
+          <Link to="login" className={styles.link}>
+            Login
+          </Link>
+          <Link to="/signup" className={styles.link}>
+            Signup
+          </Link>
+        </div>
+      )}
+      <video autoPlay muted loop playsInline>
+        <source src="/spa-video.mp4" type="video/mp4" />
+      </video>
+      <div className={styles.heroContent}>
+        <h1>Relax, Rejuvenate, and Restore</h1>
+        <p>
+          Escape the everyday and indulge in a calming spa experience designed
+          to refresh your body and mind.
+        </p>
+        <Link to="/book" className={styles.ctaButton}>
+          Book Now
+        </Link>
+      </div>
+    </section>
+  );
 }
