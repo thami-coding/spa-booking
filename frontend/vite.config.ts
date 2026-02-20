6d3bda6d344a83e010637d61ffcf9feb5f961c6c
Thami <thamig125@gmail.com>
2026-02-20 20:52:27 +0200

    remove docker config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: '0.0.0.0', // Necessary for Docker/Dev Container
-    port: 5173,
-    watch: {
-      usePolling: true,
+    allowedHosts: ["877f-168-210-88-188.ngrok-free.app"],
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
-})
+});
