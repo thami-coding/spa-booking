ce6803b84c102389d91e2fb581f764fe679f4c7d
Thami <thamig125@gmail.com>
2026-02-20 21:08:57 +0200

    remove comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,7 @@
-// import { BrowserRouter, Route, Routes } from "react-router";
-// import HomePage from "./pages/home/HomePage";
-// import BookingPage from "./pages/booking/BookingPage";
-// import LoginPage from "./pages/login/Login";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/router";
 
-import AdminDashboard from "./pages/list-dashboard/AdminDashboard";
 
-// import Dashboard from "./pages/dashboard/Dashboard";
-
-// import CheckoutPage from "./pages/checkout/CheckoutPage";
 export default function App() {
-  return (
-//  <Dashboard />
-<AdminDashboard />
-  );
+  return <RouterProvider router={router} />;
 }
